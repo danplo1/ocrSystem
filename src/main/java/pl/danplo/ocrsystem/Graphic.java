9a8aae42895dc2ffda57ca868ecd20e4ea58afd3
danplo1 <budweiser401@wp.pl>
2019-03-14 13:04:18 +0100

    add Lombok

--- src/main/java/pl/danplo/ocrsystem/Graphic.java
+++ src/main/java/pl/danplo/ocrsystem/Graphic.java
@@ -1,32 +1,24 @@
 package pl.danplo.ocrsystem;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Graphic {
 
-    @GeneratedValue
     @Id
+    @GeneratedValue
     private Long id;
     private String url;
     private String content;
 
-    public String getUrl() {
-        return url;
-    }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
--- src/main/java/pl/danplo/ocrsystem/OcrApi.java
+++ src/main/java/pl/danplo/ocrsystem/OcrApi.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class OcrApi {
 
     private OcrService ocrService;
-    private GraphicRepo graphicRepo;
+    private List<Graphic> graphicList;
 
     @Autowired
     public OcrApi(OcrService ocrService, GraphicRepo graphicRepo) {
         this.ocrService = ocrService;
-        this.graphicRepo = graphicRepo;
+        this.graphicList = new ArrayList<>();
     }
 
 
@@ -25,13 +28,14 @@ public class OcrApi {
     public String doOcr(@RequestBody Graphic graphic) {
         String ocr = ocrService.doOCR(graphic.getUrl());
         graphic.setContent(ocr);
-        graphicRepo.save(graphic);
+        graphicList.add(graphic);
         return ocr;
 
     }
 
     @GetMapping("/api/ocr")
     public Iterable<Graphic> getGraphic() {
-        return graphicRepo.findAll();
+
+        return graphicList;
     }
 }
